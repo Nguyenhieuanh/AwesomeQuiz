371a1a076a9fcc8cdb5a97c7df636c79cf6085ab
minhtn143 <minhtn143@gmail.com>
2020-07-06 09:59:00 +0700

    create categories migration

--- database/migrations/2020_07_06_022549_create_categories_table.php
+++ database/migrations/2020_07_06_022549_create_categories_table.php
@@ -14,7 +14,8 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('category_name');
             $table->timestamps();
         });
     }

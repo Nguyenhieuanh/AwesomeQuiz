75d10837af45f7af40b876a3ef2e2a800ac74289
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-06 15:37:05 +0700

    make model quiz + migration quiz + quiz controller

---
+++ database/migrations/2020_07_06_081734_create_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyInteger('duration');
+            $table->integer('question_count');
+            $table->tinyInteger('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}

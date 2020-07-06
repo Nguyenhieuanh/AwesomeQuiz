8f1d0cb508610eb7863287d52437b2746f89f85c
minhtn143 <minhtn143@gmail.com>
2020-07-06 10:24:04 +0700

    create question, category factory

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        'question_content' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'category_id' => $faker->numberBetween($min = 1, $max = 10),
+    ];
+});

a5e122a297d19c3ad32c2f0cceffabe4bab9c6b1
minhtn143 <minhtn143@gmail.com>
2020-07-07 08:19:07 +0700

    pending create view Question

--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.navbar')
 
 @section('content_home')
+<div class="col-11 mx-auto">
+    <h3 class="page-title">Question Manager</h3>
+
+    <p>
+        <a href="" class="btn btn-success">Add Question</a>
+    </p>
+    <div class="card">
+        <form>
+            @csrf
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Example select</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect2">Example multiple select</label>
+                <select multiple class="form-control" id="exampleFormControlSelect2">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </form>
+    </div>
+</div>
+
 
 @endsection

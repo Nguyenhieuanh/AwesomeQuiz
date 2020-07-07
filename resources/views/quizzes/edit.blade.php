03310c5ea12e0b51de26310e7d205b120223fc45
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-07 11:18:44 +0700

    update edit quiz form

--- resources/views/quizzes/edit.blade.php
+++ resources/views/quizzes/edit.blade.php
@@ -5,21 +5,34 @@
     <div class="card">
         <div class="card-header">
             <h4>{{ $quiz->name }}</h4>
-            {{ dd($quiz) }}
         </div>
         <div class="card-body">
             <table class="table table-striped">
                 <thead>
-                    <th>#</th>
-                    <th>Question</th>
-                    <th>Action</th>
+                    <tr>
+                        <th>#</th>
+                        <th>Question</th>
+                        <th>Action</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    @foreach ($quiz_questions as $key => $q_question)
+                    <tr>
+                        <td>{{ ++$key }}</td>
+                        <td>{{ $q_question->question->question_content }}</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-info">Detail</a> |
+                            <a href="#" class="btn btn-sm btn-danger" onclick="return alert('Are you sure?')">Delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
             </table>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-success">
+                Add more question
+            </button>
         </div>
     </div>
 </div>

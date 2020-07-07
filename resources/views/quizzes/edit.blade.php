e2132c6525d9e5b87981b8dcec87e134c0a8d061
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-07 22:06:20 +0700

    update modal add quiz

--- resources/views/quizzes/edit.blade.php
+++ resources/views/quizzes/edit.blade.php
@@ -22,7 +22,8 @@
                         <td>{{ $q_question->question->question_content }}</td>
                         <td>
                             <a href="#" class="btn btn-sm btn-info">Detail</a> |
-                            <a href="{{ route('quizQuestion.destroy', ['id' => $q_question->id]) }}" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</a>
+                            <a href="{{ route('quizQuestion.destroy', ['id' => $q_question->id]) }}"
+                                class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</a>
                         </td>
                     </tr>
                     @endforeach
@@ -42,16 +43,31 @@
 <!-- Modal -->
 <div class="modal fade" id="addQuestion" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Add question</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <form action="#" method="POST">
+                    <input type="hidden" name="quiz_id" value="{{ $quiz->id }}">
+                    @foreach ($question as $question)
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <input type="checkbox" name="question_id" value="{{ $question->id }}" aria-label="Checkbox for following text input">
+                            </div>
+                        </div>
+                        <textarea class="form-control" aria-label="Text input with checkbox" cols="5" readonly>
+                            {{ $question->question_content }}
+                        </textarea>
+                    </div>
+                    @endforeach
+                    <button class="btn btn-success" type="submit">Add</button>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>

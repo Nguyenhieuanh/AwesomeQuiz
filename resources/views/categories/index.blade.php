212e1ed38f2794b637aa2269811507160e7aa140
minhtn143 <minhtn143@gmail.com>
2020-07-08 17:37:32 +0700

    add confirm to Delete Button on Index

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,41 +1,43 @@
 @extends('layouts.navbar')
 
 @section('content_home')
-    <h3 class="page-title">Category</h3>
+<h3 class="page-title">Category</h3>
 
-    <p>@if (Auth::user()->role == 1)
-        <a href="{{ route('categories.create') }}" class="btn btn-success">Create new category</a>@endif
-    </p>
+<p>@if (Auth::user()->role == 1)
+    <a href="{{ route('categories.create') }}" class="btn btn-success">Create new category</a>@endif
+</p>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            Category List
-        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Category List
+    </div>
 
-        <div class="panel-body">
-            <table class="table table-bordered table-striped {{ count($categories) > 0 ? 'datatable' : '' }} dt-select">
-                <thead>
+    <div class="panel-body">
+        <table class="table table-bordered table-striped {{ count($categories) > 0 ? 'datatable' : '' }} dt-select">
+            <thead>
                 <tr>
                     <th>ID</th>
                     <th>Category Title</th>
                     <th>Description</th>
                     <th>Actions</th>
                 </tr>
-                </thead>
+            </thead>
 
-                <tbody>
+            <tbody>
                 @if (count($categories) > 0)
-                    @foreach ($categories as $category)
-                        <tr data-entry-id="{{ $category->id }}">
-                            <td>{{$category->id}}</td>
-                            <td><a href="{{route('categories.show',[$category->id])}}" >{{ $category->category_name }}</a></td>
-                            <td>{{$category->category_description}}</td>
-                            <td>
-                                <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-xs btn-primary">Detail</a>
-                                @if (Auth::user()->role == 1)
-                                <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-xs btn-info">Edit</a>
-                                <a href="{{ route('categories.destroy',[$category->id]) }}" class="btn btn-xs btn-danger">Delete</a>
-                                {{-- {!! Form::open(array(
+                @foreach ($categories as $category)
+                <tr data-entry-id="{{ $category->id }}">
+                    <td>{{$category->id}}</td>
+                    <td><a href="{{route('categories.show',[$category->id])}}">{{ $category->category_name }}</a></td>
+                    <td>{{$category->category_description}}</td>
+                    <td>
+                        <a href="{{ route('categories.show',[$category->id]) }}"
+                            class="btn btn-xs btn-primary">Detail</a>
+                        @if (Auth::user()->role == 1)
+                        <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-xs btn-info">Edit</a>
+                        <a href="{{ route('categories.destroy',[$category->id]) }}" class="btn btn-xs btn-danger"
+                            onclick="return confirm('Are you sure?')">Delete</a>
+                        {{-- {!! Form::open(array(
                                     'style' => 'display: inline-block;',
                                     'method' => 'DELETE',
                                     'onsubmit' => "return confirm('Are you sure?');",
@@ -43,24 +45,24 @@
                                 {!! Form::submit(trans('Delete'), array('class' => 'btn btn-xs btn-danger')) !!}
                                 @endif
                                 {!! Form::close() !!} --}}
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    {{$categories->links()}}
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+                {{$categories->links()}}
                 @else
-                    <tr>
-                        <td colspan="3">Nothing to show here</td>
-                    </tr>
+                <tr>
+                    <td colspan="3">Nothing to show here</td>
+                </tr>
                 @endif
-                </tbody>
-            </table>
-        </div>
+            </tbody>
+        </table>
     </div>
+</div>
 @stop
 
 @section('javascript')
-    <script>
-        window.route_mass_crud_entries_destroy = '{{ route('categories.mass_destroy') }}';
-    </script>
+<script>
+    window.route_mass_crud_entries_destroy = '{{ route('categories.mass_destroy') }}';
+</script>
 @endsection

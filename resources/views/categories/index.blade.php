e4f84560785b854465bfb870bffebaeb39d73a46
minhtn143 <minhtn143@gmail.com>
2020-07-09 09:34:05 +0700

    add icon to edit detail btn OK

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -31,10 +31,11 @@
                     <td><a href="{{route('categories.show',[$category->id])}}">{{ $category->category_name }}</a></td>
                     <td>{{$category->category_description}}</td>
                     <td>
-                        <a href="{{ route('categories.show',[$category->id]) }}"
-                            class="btn btn-sm btn-primary">Detail</a>
+                        <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-sm btn-primary">
+                            <span><i class="fas fa-info-circle"></i> Detail</span></a>
                         @if (Auth::user()->role == 1)
-                        <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-info">Edit</a>
+                        <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-info">
+                            <span><i class="far fa-edit"></i> Edit</a></span>
                         <button class="btn btn-sm btn-danger"
                         onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
                         <span><i class="fas fa-trash-alt"></i> Delete</span>

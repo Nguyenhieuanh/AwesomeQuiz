1dc63bb66937255f0da2a79e6597c2ffc9dd9795
minhtn143 <minhtn143@gmail.com>
2020-07-09 09:50:58 +0700

    change Btn in category list and detail

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 #### Icon Awefont List
 
 -   Delete icon:
-    <button class="btn btn-sm btn-danger"onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
-    <span><i class="fas fa-trash-alt"></i> Delete</span></button>
+    <button class="btn btn-sm btn-danger" onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
+    <span><i class="fas fa-trash-alt"></i> Delete</span>
+    </button>
 -   Edit icon:
-    <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-info">
+    <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-primary">
     <span><i class="far fa-edit"></i> Edit</a></span>
-- Detail icon:
-
+-   Detail icon:
+    <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-sm btn-info">
+    <span><i class="fas fa-info-circle"></i> Detail</span></a>
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -31,16 +31,15 @@
                     <td><a href="{{route('categories.show',[$category->id])}}">{{ $category->category_name }}</a></td>
                     <td>{{$category->category_description}}</td>
                     <td>
-                        <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-sm btn-primary">
+                        <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-sm btn-info">
                             <span><i class="fas fa-info-circle"></i> Detail</span></a>
                         @if (Auth::user()->role == 1)
-                        <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-info">
+                        <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-primary">
                             <span><i class="far fa-edit"></i> Edit</a></span>
                         <button class="btn btn-sm btn-danger"
-                        onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
-                        <span><i class="fas fa-trash-alt"></i> Delete</span>
+                            onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
+                            <span><i class="fas fa-trash-alt"></i> Delete</span>
                         </button>
-
                         @endif
                     </td>
                 </tr>

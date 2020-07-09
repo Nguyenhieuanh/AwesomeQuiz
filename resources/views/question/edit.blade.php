7172406715285d6e009c63bfa44466cc3dac2a4c
minhtn143 <minhtn143@gmail.com>
2020-07-10 02:51:24 +0700

    refactor Edit OK

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -73,7 +73,8 @@
                             {{-- End of Select Category group --}}
                         </div>
                     </div>
-                    @if ($question->answer)
+
+                    @if ($answers->isEmpty())
                     {{-- Start of Answer group  --}}
                     <div class="form-group">
                         <div class="col">
@@ -191,19 +192,23 @@
                             <div class="row">
                                 <div class="input-group">
                                     <div class="input-group-prepend">
-                                    <label class="input-group-text" for="correct_select_{{$key}}"> <strong> Answer Option {{$key}}
+                                        <label class="input-group-text" for="correct_select_{{$key+1}}"> <strong> Answer
+                                                Option {{$key+1}}
                                             </strong>
                                         </label>
                                     </div>
-                                <select class="custom-select" id="correct_select_{{$key}}" name="correct_option[]">
-                                        <option class="text-danger" value="0" {{($answer->correct == 0) ? 'selected' : ''}}>Wrong</option>
-                                        <option class="text-success" value="1" {{($answer->correct == 1) ? 'selected' : ''}}>Right</option>
+                                    <select class="custom-select" id="correct_select_{{$key+1}}" name="correct_option[]">
+                                        <option class="text-danger" value="0"
+                                            {{($answer->correct == 0) ? 'selected' : ''}}>Wrong</option>
+                                        <option class="text-success" value="1"
+                                            {{($answer->correct == 1) ? 'selected' : ''}}>Right</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
                         <textarea class="form-control @error('answer_content[]') is-invalid @enderror"
-                            id="answer_content_1" name="answer_content[]" rows="2" required></textarea>
+                            id="answer_content_1" name="answer_content[]" rows="2" required>{{ $answer->answer_content }}
+                        </textarea>
                         @error('answer_content[]')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{$message}}</strong>
@@ -211,11 +216,8 @@
                         @enderror
                     </div>
                     {{-- End of Answer group  --}}
-                        @endforeach
+                    @endforeach
                     @endif
-
-
-
                     <div class="col-5">
                         <button type="submit" class="btn btn-success">
                             <span> <i class="fas fa-save"></i> Save </span>
@@ -228,92 +230,6 @@
                     </div>
                 </form>
             </div>
-
-            <form method="POST" action="{{route('question.update',$question->id)}}">
-                @csrf
-                <div class="form-group">
-                    <label for="question_content">Question</label>
-                    <textarea class="form-control @error('question_content') is-invalid @enderror" id="question_content"
-                        name="question_content" rows="5">{{$question->question_content}}</textarea>
-                    @error('question_content')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{$message}}</strong>
-                    </span>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="difficulty">Difficulty</label>
-                    <select class="form-control" id="difficulty" name="difficulty">
-                        <option value="{{$question->difficulty}}">
-                            @switch($question->difficulty)
-                            @case(1)
-                            <td>
-                                <p class="text-success">Easy</p>
-                            </td>
-                            @break
-                            @case(2)
-                            <td>
-                                <p class="text-warning">Medium</p>
-                            </td>
-                            @break
-                            @case(3)
-                            <td>
-                                <p class="text-danger">Hard</p>
-                            </td>
-                            @break
-                            @endswitch</option>
-                        <option value="1">Easy</option>
-                        <option value="2">Medium</option>
-                        <option value="3">Hard</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="category">Category</label>
-                    <select class="form-control" id="category" name="category">
-                        <option value="{{$question->category_id}}">{{$question->category->category_name}}</option>
-                        @foreach ($categories as $category)
-                        <option value="{{$category->category_name}}">{{$category->category_name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-row">
-                    <div class="col-2">
-                        <label for="answer_option_1">Answer Option 1</label>
-                    </div>
-                    @foreach($answers as $key => $answer)
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <select name="correct_option[]">
-                                    <option value="1" {{ $answer->correct == 1 ? 'selected' : '' }}>Right</option>
-                                    <option value="0" {{ $answer->correct == 0 ? 'selected' : ''}}>Wrong</option>
-                                </select>
-
-                            </div>
-                        </div>
-                        <textarea class="form-control @error(" answer_option_". ++$key) is-invalid @enderror"
-                            id="answer_option_1" name="answer_content[]"
-                            rows="2">{{ $answer->answer_content }}</textarea>
-                        @error('answer_option_'.++$key)
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{$message}}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                    @endforeach
-                </div>
-
-
-                <div class="col-5">
-                    <button type="submit" class="btn btn-success">
-                        <span> <i class="fas fa-save"></i> Save </span>
-                    </button>
-                    <button type="button" class="btn btn-dark" onclick="window.history.back()">
-                        <span> <i class="fas fa-arrow-left"></i> Back</span>
-                    </button>
-                </div>
-            </form>
         </div>
     </div>
 </div>

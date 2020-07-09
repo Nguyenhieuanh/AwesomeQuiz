62bf1cef59851945cee5de754a29eeff2464e545
minhtn143 <minhtn143@gmail.com>
2020-07-10 02:26:38 +0700

    select category OK

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -36,9 +36,12 @@
                                             </label>
                                         </div>
                                         <select class="form-control" id="difficulty" name="difficulty">
-                                            <option value="1" {{($question->difficulty == 1) ? 'selected' : ''}}>Easy</option>
-                                            <option value="2" {{($question->difficulty == 2) ? 'selected' : ''}}>Medium</option>
-                                            <option value="3" {{($question->difficulty == 3) ? 'selected' : ''}}>Hard</option>
+                                            <option value="1" {{($question->difficulty == 1) ? 'selected' : ''}}>Easy
+                                            </option>
+                                            <option value="2" {{($question->difficulty == 2) ? 'selected' : ''}}>Medium
+                                            </option>
+                                            <option value="3" {{($question->difficulty == 3) ? 'selected' : ''}}>Hard
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -58,7 +61,8 @@
                                         </div>
                                         <select class="custom-select" id="category-select" name="category" required>
                                             @foreach ($categories as $category)
-                                            <option value="{{$category->id}}">{{$category->category_name}}</option>
+                                            <option value="{{$category->id}}" {{($category->id == $question->category->id) ? 'selected' : ''}}>{{$category->category_name}}
+                                                </option>
                                             @endforeach
                                         </select>
                                     </div>

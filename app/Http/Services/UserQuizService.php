453d990a156915d7283288958fb11172450ceabf
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-09 15:01:22 +0700

    fix QuizService

--- app/Http/Services/UserQuizService.php
+++ app/Http/Services/UserQuizService.php
@@ -2,7 +2,53 @@
 
 namespace App\Http\Services;
 
+use App\Http\Repositories\UserQuizRepo;
+
 class UserQuizService
 {
-    
+    protected $userQuizRepo;
+
+    public function __construct(UserQuizRepo $userQuizRepo)
+    {
+        $this->userQuizRepo = $userQuizRepo;
+    }
+    public function getAll()
+    {
+        return $this->userQuizRepo->getAll();
+    }
+
+    public function findById($id)
+    {
+        $userQuiz = $this->userQuizRepo->findById($id);
+
+        return (!$userQuiz ? abort(404) : $userQuiz);
+    }
+
+    public function create($request)
+    {
+        return $this->userQuizRepo->create($request);
+    }
+
+    public function update($id, $request)
+    {
+        $oldUserQuiz = $this->userQuizRepo->findById($id);
+
+        if (!$oldUserQuiz) {
+            if (!$oldUserQuiz) {
+                abort(404);
+            } else {
+                return  $this->quizRepo->update($request, $oldUserQuiz);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $quiz = $this->quizRepo->findById($id);
+
+        if ($quiz) {
+            return $this->quizRepo->destroy($quiz);
+        }
+        return 404;
+    }
 }

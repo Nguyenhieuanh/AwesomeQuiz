914800576e7a1d8bf4fc4794688f44c9081ce97a
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-11 01:38:01 +0700

    create new css file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -27,6 +27,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
 
+    <link rel="stylesheet" href="{{ asset('sass/customCSS.css') }}">
+
     <!-- App CSS -->
     <link type="text/css" href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script>

8b5d6ccb2486003df8a6bb5ddd740c7f3fb5badc
minhtn143 <minhtn143@gmail.com>
2020-07-10 11:26:50 +0700

    updated  list category

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -5,53 +5,80 @@
     <div class="card">
         <div class="card-header">
             <h3 class="page-title">Category</h3>
-            <p>@if (Auth::user()->role == 1)
-                <a href="{{ route('categories.create') }}" class="btn btn-success">Create new category</a>@endif
-            </p>
-            <div class="card-body p-4">
-                <table
-                    class="table table-bordered table-hover {{ count($categories) > 0 ? 'datatable' : '' }}">
-                    <thead>
-                        <tr class="table-primary">
-                            <th>ID</th>
-                            <th>Category Title</th>
-                            <th>Description</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @if (count($categories) > 0)
-                        @foreach ($categories as $category)
-                        <tr data-entry-id="{{ $category->id }}">
-                            <td>{{$category->id}}</td>
-                            <td><a
-                                    href="{{route('categories.show',[$category->id])}}">{{ $category->category_name }}</a>
-                            </td>
-                            <td>{{$category->category_description}}</td>
-                            <td>
-                                <a href="{{ route('categories.show',[$category->id]) }}" class="btn btn-sm btn-info">
-                                    <span><i class="fas fa-info-circle"></i> Detail</span></a>
-                                @if (Auth::user()->role == 1)
-                                <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-primary">
-                                    <span><i class="far fa-edit"></i> Edit</a></span>
-                                <button class="btn btn-sm btn-danger"
-                                    onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
-                                    <span><i class="fas fa-trash-alt"></i> Delete</span>
-                                </button>
-                                @endif
-                            </td>
-                        </tr>
-                        @endforeach
-                        {{$categories->links()}}
-                        @else
-                        <tr>
-                            <td colspan="3">Nothing to show here</td>
-                        </tr>
-                        @endif
-                    </tbody>
-                </table>
+        </div>
+        <div class="card-body p-4">
+            <div class="float-left">@if (Auth::user()->role == 1)
+                <a href="{{ route('categories.create') }}" class="btn btn-success">
+                    <span><i class="fas fa-plus"></i> Create category</span>
+                </a>@endif
+            </div>
+            <table class="table table-bordered table-hover {{ count($categories) > 0 ? 'datatable' : '' }}">
+                <thead>
+                    <tr class="table-primary">
+                        <th>ID</th>
+                        <th style="width: 15%">Category Title</th>
+                        <th>Description</th>
+                        <th style="width: 15%">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if (count($categories) > 0)
+                    @foreach ($categories as $category)
+                    <tr data-entry-id="{{ $category->id }}">
+                        <td> {{$category->id}}</td>
+                        <td data-toggle="modal" data-target="#category-details" title="Click for more Details">
+                            <p>{{ $category->category_name }}</p>
+                        </td>
+                        <td>{{$category->category_description}}</td>
+                        <td>
+                            @if (Auth::user()->role == 1)
+                            <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-sm btn-primary">
+                                <span><i class="far fa-edit"></i> Edit</a></span>
+                            <button class="btn btn-sm btn-danger"
+                                onclick="confirmDelete('{{ route('categories.destroy',[$category->id]) }}')">
+                                <span><i class="fas fa-trash-alt"></i> Delete</span>
+                            </button>
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                    @else
+                    <tr>
+                        <td colspan="3">Nothing to show here</td>
+                    </tr>
+                    @endif
+                </tbody>
+            </table>
+            <div class="float-right">{{$categories->links()}}</div>
+        </div>
+
+    </div>
+</div>
+@endsection
+
+
+{{-- Category Detail Modal --}}
+<div class="modal fade" id="category-details" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">{{ $category->category_name }}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fas fa-times-circle"></i></span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <strong>Description</strong>
+                <p>{{$category->category_description}}</p>
+            </div>
+            <div class="modal-footer">
+                <a href="{{ route('categories.edit',[$category->id]) }}" class="btn btn-primary">
+                    <span><i class="far fa-edit"></i> Edit</a></span>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                    <span><i class="fas fa-times"></i> Close </span>
+                </button>
             </div>
         </div>
     </div>
 </div>
-@endsection
+{{-- End Category Detail Modal --}}

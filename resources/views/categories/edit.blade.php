100182ca3117aecf062f68d00a834c388bc7d272
minhtn143 <minhtn143@gmail.com>
2020-07-10 16:46:48 +0700

    edit form OK

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,39 +1,47 @@
 @extends('layouts.navbar')
 
 @section('content_home')
+<div class="col-8 mt-3 mx-auto">
+    <div class="card">
+        <div class="card-header">
+            <h3>Edit Category</h3>
+        </div>
+        <div class="card-body p-4">
+            <form method="POST" action="{{route('categories.update', $category->id)}}">
+                @csrf
+                <div class="form-group">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="category_name"> <strong> Category Name
+                            </strong>
+                        </label>
+                        <input type="text" class="form-control @error('category_name') is-invalid @enderror"
+                            id="category_name" name="category_name" autofocus value="{{$category->category_name}}">
+                        @error('category_name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <textarea class="form-control @error('category_description') is-invalid @enderror"
+                        id="category_description" name="category_description" rows="5"
+                        placeholder="Category Description">{{$category->category_description}}</textarea>
+                    @error('category_description')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{$message}}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="float-right">
+                    <button type="submit" class="btn btn-success">
+                        <span> <i class="fas fa-save"></i> Save </span>
+                    </button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-secondary">
+                        <span> <i class="fas fa-arrow-left"></i> Back</span>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-
-
-    <fieldset><legend><h3 class="page-title">Update this category</h3></legend>
-        {!! Form::model($category, ['method' => 'POST', 'route' => ['categories.update', $category->id]]) !!}
-            @csrf
-
-
-
-
-                        <div class="form-group">
-                            <label>Category name
-                                <input type="text" placeholder="New category name" class="form-control" name="category_name" value="{{$category->category_name}}">
-                            </label>
-                            <p class="help-block"></p>
-                            @if($errors->has('category_name'))
-                                <p class="help-block">
-                                    {{ $errors->first('category_name') }}
-                                </p>
-                            @endif
-                            <br>
-
-                        </div>
-
-
-
-
-            <div class="md-form">
-                <label for="form7">Category description</label><textarea id="form7" placeholder="Category description" class="md-textarea form-control" name="category_description" rows="3" >{{$category->category_description}}</textarea>
-
-            </div>
-    </fieldset>
-    {!! Form::submit(trans('Update it!'), ['class' => 'btn btn-danger']) !!}
-    {!! Form::close() !!}
-@stop
-
+@endsection

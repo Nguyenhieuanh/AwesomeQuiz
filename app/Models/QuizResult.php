da3589c059c1e325b49f590849e1c493fda353a5
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-10 14:03:02 +0700

    add user_quiz_id

--- app/Models/QuizResult.php
+++ app/Models/QuizResult.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class QuizResult extends Model
 {
     protected $fillable = [
-        'user_id', 'quiz_id', 'question_id', 'answer_id', 'correct', 'answered'
+        'user_id', 'quiz_id', 'question_id', 'answer_id', 'correct', 'answered', 'user_quiz_id'
     ];
 
     public function users()

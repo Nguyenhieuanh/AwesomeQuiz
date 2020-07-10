19a01ed0917a42e677231ad7a4f503e0a70de383
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-11 01:42:26 +0700

    add js for dynamic input

--- public/js/custom/customJs.js
+++ public/js/custom/customJs.js
@@ -1,4 +1,3 @@
-
 /* -------------------------------------------------------------------------- */
 /*                            Alert confirm delete                            */
 /* -------------------------------------------------------------------------- */
@@ -13,7 +12,10 @@ const swalWithBootstrapButtons = Swal.mixin({
     buttonsStyling: false
 });
 
-function confirmDelete(url_link, message="You won't be able to revert this!") {
+function confirmDelete(
+    url_link,
+    message = "You won't be able to revert this!"
+) {
     swalWithBootstrapButtons
         .fire({
             title: "Are you sure?",
@@ -54,11 +56,90 @@ $(
 /* ----------------------------------- End ---------------------------------- */
 
 $(document).ready(function() {
-    $(".deactivate").prop("disabled", false);
-    $(".checkboxes").on("change", function() {
+    var i = 1;
+
+    // Add answer option
+    $(document).on("click", "#add-answer", function() {
+        i++;
+        $("#dynamic-field").append(
+            '<div class="form-group">' +
+                '<label for="password">Answer option #' +
+                i +
+                ":</label>" +
+                '<div class="input-group">' +
+                '<div class="input-group-prepend">' +
+                '<div class="input-group-text" id="basic-addon1">' +
+                '<input type="hidden" name="corrects[]" class="deactivate" value="0">' +
+                '<input class="inp-cbx checkboxes" id="cbx' +
+                i +
+                '" data-id="' +
+                i +
+                '" type="checkbox" name="corrects[]" style="display: none" value="1" />' +
+                '<label class="cbx" for="cbx' +
+                i +
+                '">' +
+                '<span class="bg-light" title="Correct answer">' +
+                '<svg width="12px" height="10px" viewbox="0 0 12 10">' +
+                '<polyline points="1.5 6 4.5 9 10.5 1"></polyline>' +
+                "</svg>" +
+                "</span>" +
+                "</label>" +
+                "</div>" +
+                "</div>" +
+                '<textarea class="form-control answers" name="answer_content[]" rows="2"></textarea>' +
+                '<div class="input-group-append">' +
+                '<span role="button" class="input-group-text remove" title="Delete">' +
+                '<i class="far fa-trash-alt"></i></span>' +
+                "</div>" +
+                '<div class="invalid-feedback">This is a required field.</div>' +
+                "</div>" +
+                "</div>"
+        );
+    });
+
+    // Remove answer option
+    $(document).on("click", ".remove", function() {
         $(this)
-            .next()
-            .prop("disabled", $(this).prop("checked"));
+            .closest("div.form-group")
+            .remove();
+        i--;
+    });
+
+    // Check right/wrong answer
+    $(document).on("change", ".checkboxes", function() {
+        $(this).prop("checked")
+            ? $(this)
+                  .prev()
+                  .prop("disabled", true)
+            : $(this)
+                  .prev()
+                  .prop("disabled", false);
+        $(this).prop("checked")
+            ? $(this)
+                  .next()
+                  .children()
+                  .removeClass("bg-light")
+            : $(this)
+                  .next()
+                  .children()
+                  .addClass("bg-light");
+    });
+
+    // Validate answer option
+    $(document).on("click", "#btn-submit", function() {
+        var count = 0;
+        $(".answers").each(function() {
+            if ($(this).val() == "") {
+                $(this).addClass("is-invalid");
+                count++;
+            } else {
+                $(this).removeClass("is-invalid");
+            }
+        });
+        if (count == 0) {
+            $("#myForm").submit();
+        }
+        console.log(count);
     });
 
     $("#checkAll").click(function() {
@@ -73,7 +154,8 @@ $(document).ready(function() {
     if (localStorage.minutes && localStorage.seconds && localStorage.start) {
         let d = Math.floor(new Date().getTime() / 1000);
         let ran_time = d - localStorage.start;
-        let remain_time = localStorage.minutes * 60 + parseInt(localStorage.seconds);
+        let remain_time =
+            localStorage.minutes * 60 + parseInt(localStorage.seconds);
         let real_time = remain_time - ran_time;
         var minutes = Math.floor(real_time / 60);
         var seconds = Math.floor(real_time % 60);

7076861e48d25e491ed059786c72872efdbb6fd7
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-12 15:50:22 +0700

    update view show users statistical results

--- app/Http/Controllers/QuizResultController.php
+++ app/Http/Controllers/QuizResultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Services\UserQuizService;
@@ -50,6 +51,12 @@ class QuizResultController extends Controller
 
     public function showUserResults($userId)
     {
-        return (Auth::id() == $userId) ? view('user_quiz.statistical') : abort(403);
+        $user = User::find($userId);
+        $userQuizzes = $user->userQuizzes;
+
+        return
+        (Auth::id() == $userId || Auth::user()->role != 0) ?
+        view('user_quiz.statistical', compact('user', 'userQuizzes')) :
+        abort(403);
     }
 }
--- resources/views/user_quiz/statistical.blade.php
+++ resources/views/user_quiz/statistical.blade.php
@@ -3,10 +3,10 @@
 @section('content_home')
 <div class="card">
     <div class="card-header">
-        <h4>My Results</h4>
+        <h4>User: {{ $user->email }}</h4>
     </div>
     <div class="card-body">
-        <table class="table table-striped">
+        <table class="table table-striped table-bordered">
             <thead>
                 <th>Quiz</th>
                 <th>Date</th>
@@ -14,12 +14,14 @@
                 <th></th>
             </thead>
             <tbody>
+                @foreach ($userQuizzes as $userQuiz)
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>{{ $userQuiz->quiz->name }}</td>
+                    <td>{{ $userQuiz->quiz->created_at }}</td>
                     <td></td>
                     <td><button class="btn-sm btn-info"><i class="fas fa-info-circle"></i> Detail</button></td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
     </div>

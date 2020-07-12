3deeca66661cfd2bdbec329750e850dc8cd98fed
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-12 15:34:13 +0700

    udpate view statistical

--- resources/views/user_quiz/statistical.blade.php
+++ resources/views/user_quiz/statistical.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.navbar')
 
 @section('content_home')
-statistical work!
+<div class="card">
+    <div class="card-header">
+        <h4>My Results</h4>
+    </div>
+    <div class="card-body">
+        <table class="table table-striped">
+            <thead>
+                <th>Quiz</th>
+                <th>Date</th>
+                <th>Result</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button class="btn-sm btn-info"><i class="fas fa-info-circle"></i> Detail</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection

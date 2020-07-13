37a8e5b869989b704103d95d51ddb4a0f8229773
minhtn143 <minhtn143@gmail.com>
2020-07-13 16:19:09 +0700

    delete old view

--- resources/views/quizzes/list.blade.php
+++ resources/views/quizzes/list.blade.php
@@ -98,7 +98,7 @@
                         <td scope="row">
                             {{-- Button Group --}}
                             {{-- <a href="{{ route('quiz.show',[$quiz->id]) }}" class="btn btn-sm btn-info">
-                                <span><i class="fas fa-info-circle"></i> Detail</span></a> --}}
+                            <span><i class="fas fa-info-circle"></i> Detail</span></a> --}}
                             @if (Auth::user()->role == 2)
                             <a href="{{ route('quiz.edit',[$quiz->id]) }}" class="btn
                         btn-sm btn-primary"> <span><i class="far fa-edit"></i></span> Edit</a>
@@ -120,48 +120,4 @@
     </div>
 </div>
 </div>
-{{-- Old --}}
-{{-- <div class="col-12 p-4">
-        @if (Auth::user()->role == 2 || Auth::user()->role ==1)
-            <a href="{{ route('quiz.create') }}" class="btn btn-success mb-4">Create new Quiz</a>
-@endif
-<div class="card-group">
-    @foreach ($quizzes as $quiz)
-    <div class="col-4">
-        <div class="card mb-3">
-            <div class="card-header">
-                <h3>
-                    <a href="{{ route('quiz.show', ['id' => $quiz->id]) }}">{{ $quiz->name }}</a>
-                    - @if (Auth::user()->role == 1 ||Auth::user()->role == 2)
-                    <a class="btn-warning btn" href="{{ route('quiz.edit', ['id' => $quiz->id]) }}">Edit</a>
-                    @endif
-                </h3>
-            </div>
-            <div class="card-body">
-                <p>
-                    Number of quizzes: {{ $quiz->quizz_count }}
-                </p>
-                <p>
-                    Duration: {{ $quiz->duration }} minutes
-                </p>
-                <p>
-                    @foreach ($quiz->quizquizzes as $q_quizz)
-                    {{ ($q_quizz->question->difficulty == 1) }}
-                    @endforeach
-                </p>
-                <a class="btn btn-info" href="{{route('quiz.statistics',['id'=>$quiz->id])}}">Show
-                    statistics</a>
-            </div>
-            <div class="card-footer">
-                @if (Auth::user()->role == 0)
-                <a href="{{ route('quiz.doQuiz', ['id' => $quiz->id]) }}" class="btn btn-success">Do
-                    Quiz</a>
-                @endif
-            </div>
-        </div>
-    </div>
-    @endforeach
-</div>
-{{ $quizzes->links() }}
-</div> --}}
 @endsection
